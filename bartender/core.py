340e2ef7a048052b91f82c1d8d9c65ba9aa3ea77
Heinz Eckert <heinz_eckert@hotmail.com>
2022-11-27 12:50:36 +0100

    docstring for _parse_axs staticmethod

--- bartender/core.py
+++ bartender/core.py
@@ -1,9 +1,8 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
-from typing import List
+from typing import Dict
 
 from bartender import test_df
 from bartender.aggregate import Aggregator
@@ -23,55 +22,73 @@ class GridPlot:
         self.ncols = gridconf.ncols
 
         # Set up grid based on gridconf.
-        self.fig, axes = plt.subplots(**gridconf)
+        self.fig, axs = plt.subplots(**gridconf)
 
-        # Extract AxesSubplots from np.ndarray
+        # Extract axsSubplots from np.ndarray
         # and map them to Aggregator attributes.
-        self.axes_map = self._parse_axes(axes, self.nrows, self.ncols)
+        self.axs_map = self._parse_axs(axs, self.nrows, self.ncols)
 
     @staticmethod
-    def _parse_axes(axes, nrows, ncols) -> None:
-        """Map keys to AxesSubplots returned by `plt.subplots`"""
+    def _parse_axs(
+        axs, nrows: int, ncols: int
+    ) -> Dict[str, matplotlib.axes.Axes]:
+        """Parse `plt.subplots` result and assign keys to the AxesSubplots.
 
-        axes_map = {
+        Depending on number of rows and columns, plt.subplots returns
+        either a single AxesSubplot or a numpy ndarray containing multiple
+        subplot objects. This function parses the return object and maps it
+        to keys, refering to the corresponding `Aggregator` attributes.
+        For details see
+        https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.subplots.html
+
+        Arguments:
+            axs: Second output elemeent of `plt.subplots`.
+            nrows (int): Number of rows in desired plot.
+            ncols (int): Number of columns in desired plot.
+
+        Returns:
+            dict: A dictionary mapping string keys to the AxesSubplots.
+        """
+
+        axs_map = {
             'overall_pct': None,
             'overall_avg': None,
             'group_pct': None,
             'group_avg': None
         }
 
-        if not isinstance(axes, np.ndarray):
-            axes_map['group_pct'] = axes
+        if not isinstance(axs, np.ndarray):
+            axs_map['group_pct'] = axs
 
         else:
             if sum([nrows, ncols]) < 4:
                 if nrows > 1:
-                    axes = axes.reshape((2, 1))
+                    axs = axs.reshape((2, 1))
 
                 if ncols > 1:
-                    axes = axes.reshape((1, 2))
+                    axs = axs.reshape((1, 2))
 
-            group_axes = axes[-1, ]
+            group_axs = axs[-1, ]
 
-            if len(group_axes) > 1:
-                axes_map['group_pct'] = group_axes[0]
-                axes_map['group_avg'] = group_axes[1]
+            if len(group_axs) > 1:
+                axs_map['group_pct'] = group_axs[0]
+                axs_map['group_avg'] = group_axs[1]
             else:
-                axes_map['group_pct'] = group_axes[0]
+                axs_map['group_pct'] = group_axs[0]
 
             if nrows > 1:
-                overall_axes = axes[0, ]
+                overall_axs = axs[0, ]
 
-                if len(overall_axes) > 1:
-                    axes_map['overall_pct'] = overall_axes[0, ]
-                    axes_map['overall_avg'] = overall_axes[1, ]
+                if len(overall_axs) > 1:
+                    axs_map['overall_pct'] = overall_axs[0, ]
+                    axs_map['overall_avg'] = overall_axs[1, ]
                 else:
-                    axes_map['overall_pct'] = overall_axes[0]
+                    axs_map['overall_pct'] = overall_axs[0]
 
-        return axes_map
+        return axs_map
 
     def show(self):
-        for key, ax in self.axes_map.items():
+        for key, ax in self.axs_map.items():
             if ax is not None:
                 data = self.aggregator.__getattribute__(key)
                 data.plot(kind='barh', stacked=True, ax=ax)
@@ -86,6 +103,7 @@ def main():
         groupby='group',
         count='bins',
         average='metric',
+        average_type='median',
         overall=True
     )
 
