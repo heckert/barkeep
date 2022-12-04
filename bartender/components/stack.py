c93a9b0852ca2e520b1e4acbb70888341e04258d
Heinz Eckert <heinz_eckert@hotmail.com>
2022-12-04 12:48:27 +0100

    linting in stack.py

--- bartender/components/stack.py
+++ bartender/components/stack.py
@@ -13,37 +13,34 @@ def annotate_bars(
     annotation_threshold: float = cfg.annotation_threshold
 ) -> None:
 
-
     for p in ax.patches:
         width, height = p.get_width(), p.get_height()
         if width < annotation_threshold:
             continue
         x, y = p.get_xy()
 
-        ax.text(x+width/2, 
-                y+height/2, 
-                '{:.1f}'.format(width*100), 
-                horizontalalignment='center', 
+        ax.text(x + width / 2,
+                y + height / 2,
+                '{:.1f}'.format(width * 100),
+                horizontalalignment='center',
                 verticalalignment='center',
                 fontname=cfg.font.name,
                 fontsize=cfg.font.size.s)
 
 
 def plot_stack(df: pd.DataFrame,
-               ax: matplotlib.axes.Axes = None):
+               ax: matplotlib.axes.Axes = None) -> None:
 
     df.plot(kind='barh', stacked=True, ax=ax)
 
-    # If no ax is passed,
+    # If no ax is passed as parameter,
     # get Axes object manually.
     if ax is None:
         ax = plt.gca()
 
     annotate_bars(ax)
 
-    ax.set_xlim(0,1)
-
-
+    ax.set_xlim(0, 1)
 
 
 if __name__ == '__main__':
@@ -51,7 +48,5 @@ if __name__ == '__main__':
     from bartender import test_agg
 
     plot_stack(test_agg)
-    
+
     plt.show()
-
-
