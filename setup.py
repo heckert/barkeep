2cc3681471159897d0eb63362669153a5002d3da
Heinz Eckert <heinz.eckert@magenta.at>
2022-12-22 13:59:36 +0100

    added manifest & setup updates

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='Annotated barplots in a nice grid',
     author='Heinz Eckert',
     author_email='heinzeckert6@gmail.com',
-    packages=find_namespace_packages(),
+    packages=find_namespace_packages(['barkeep', 'barkeep.*']),
     include_package_data=True,
     python_requires='>=3.10',
     install_requires=install_requires
